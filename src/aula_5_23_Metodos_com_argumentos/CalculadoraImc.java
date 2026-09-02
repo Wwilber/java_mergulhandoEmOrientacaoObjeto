0561ac916b57562d49a0f6486ac7e0d63dcdf3a7
Wilber <wilber.programacao@gmail.com>
2026-09-02 13:17:19 -0300

    aula_5_23_Metodos_com_argumentos

--- src/aula_5_23_Metodos_com_argumentos/CalculadoraImc.java
+++ src/aula_5_23_Metodos_com_argumentos/CalculadoraImc.java
@@ -1,11 +1,10 @@
 package aula_5_23_Metodos_com_argumentos;
+
 //
 public class CalculadoraImc {
 
-    double altura;
-    double peso;
 
-    IndiceMassaCorporal calcular(){
+    IndiceMassaCorporal calcular(double altura, double peso) {
         double imcCalculado = peso / (altura * altura);
 
         IndiceMassaCorporal imc = new IndiceMassaCorporal();
