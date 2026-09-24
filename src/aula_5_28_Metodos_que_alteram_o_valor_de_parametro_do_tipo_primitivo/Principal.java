62327cace97c44c424b0359d8388e1ed2526c598
Wilber <wilber.programacao@gmail.com>
2026-09-24 13:39:03 -0300

    aula_5_28_Metodos_que_alteram_o_valor_de_parametro_do_tipo_primitivo

--- src/aula_5_28_Metodos_que_alteram_o_valor_de_parametro_do_tipo_primitivo/Principal.java
+++ src/aula_5_28_Metodos_que_alteram_o_valor_de_parametro_do_tipo_primitivo/Principal.java
@@ -2,6 +2,16 @@ package aula_5_28_Metodos_que_alteram_o_valor_de_parametro_do_tipo_primitivo;
 
 public class Principal {
     public static void main(String[] args) {
+        double precoCompraFornecedor = 140;
+
+        ServicoDePrecificacao servicoDePrecificacao = new ServicoDePrecificacao();
+        servicoDePrecificacao.calcularPrecoVenda(precoCompraFornecedor);
+
+        double venda2 = servicoDePrecificacao.calcularPrecoVenda2(precoCompraFornecedor);
+
+        System.out.printf("Preço: %.2f%n", precoCompraFornecedor);
+        System.out.printf("Preço: %.2f%n", venda2);
+
 
     }
 }
--- src/aula_5_28_Metodos_que_alteram_o_valor_de_parametro_do_tipo_primitivo/ServicoDePrecificacao.java
+++ src/aula_5_28_Metodos_que_alteram_o_valor_de_parametro_do_tipo_primitivo/ServicoDePrecificacao.java
@@ -1,7 +1,13 @@
 package aula_5_28_Metodos_que_alteram_o_valor_de_parametro_do_tipo_primitivo;
 
 public class ServicoDePrecificacao {
-    void calcularPrecoVenda(double precoCusto){
+    void calcularPrecoVenda(double precoCusto) {
+        precoCusto = precoCusto * 1.20;
+
+    }
+
+    double calcularPrecoVenda2(double precoCusto) {
+        return precoCusto = precoCusto * 1.20;
 
     }
 }
